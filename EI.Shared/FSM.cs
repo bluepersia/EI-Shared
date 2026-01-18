d056c1ae2f4468a59cd11f3ed1f6af31ffddf88b
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-17 19:01:34 -0600

    FSM draft

---
+++ EI.Shared/FSM.cs
@@ -0,0 +1,105 @@
+﻿namespace EI.Shared;
+
+public class FSM
+{
+    public int Epoch { get; private set; } = 0;
+    public int CurrentStateId { get; private set; } = -1;
+
+    private Dictionary<int, List<FSMState>> _states = new Dictionary<int, List<FSMState>>();
+    public void Register(int stateId, FSMState state)
+    {
+        if (!_states.ContainsKey(stateId))
+        {
+            _states[stateId] = new List<FSMState>();
+        }
+        _states[stateId].Add(state);
+    }
+
+    public T? GetState<T>(int stateId) where T : FSMState
+    {
+        if (!_states.ContainsKey(stateId))
+        {
+            return null;
+        }
+        return _states[stateId].OfType<T>().FirstOrDefault();
+    }
+
+    public void SetState(int stateId, bool preserveEpoch = false)
+    {
+        if (CurrentStateId == stateId)
+        {
+            return;
+        }
+
+        EnterState(stateId, preserveEpoch);
+    }
+
+    public void EnterState(int stateId, bool preserveEpoch = false)
+    {
+        if (!_states.ContainsKey(stateId))
+        {
+            return;
+        }
+
+        if (_states.ContainsKey(CurrentStateId))
+        {
+            _states[CurrentStateId].ForEach(state => state.Exit());
+        }
+
+        if (!preserveEpoch)
+        {
+            Epoch++;
+        }
+        CurrentStateId = stateId;
+        _states[CurrentStateId].ForEach(state => state.Enter());
+    }
+
+
+
+    public void Update()
+    {
+        if (!_states.ContainsKey(CurrentStateId))
+            return;
+
+        _states[CurrentStateId].ForEach(state => state.Update());
+    }
+}
+
+
+public class FSM<T> : FSM
+{
+    public T Owner { get; private set; }
+    public FSM(T owner)
+    {
+        Owner = owner;
+    }
+}
+
+public abstract class FSMState
+{
+
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+}
+
+public abstract class FSMState<T> : FSMState
+{
+    public T Parent { get; private set; }
+
+    public FSMState(T parent)
+    {
+        Parent = parent;
+    }
+}
